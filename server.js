02f18b48069de430f0ee914cf7963da301f9a0c3
NewGitRepository <prachigupta0527@gmail.com>
2020-09-20 22:18:28 +0530

    updated server

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/blogApp'));
+app.use(express.static('./dist/garlicapp'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/blogApp/index.html'));
+res.sendFile(path.join(__dirname,'/dist/garlicapp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
